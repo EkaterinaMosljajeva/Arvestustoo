18bfcffcd5e46e383c138b0334af361b4d3566ad
EkaterinaMosljajeva <120181296+EkaterinaMosljajeva@users.noreply.github.com>
2023-03-29 23:36:03 +0300

    Update Arvestustoo.py

--- Arvestustoo/Arvestustoo.py
+++ Arvestustoo/Arvestustoo.py
@@ -25,64 +25,63 @@ def vaata_nimekirja(event):
 
 def poisk(event):
     def poisk2():
-        h=str(ent_poisk.get())
-        val=var.get()
-        if val==1:
-            lbl_otvet.configure(aken_poisk,text=riik_peal[h])
-        elif val==2:
-            lbl_otvet.configure(aken_poisk,text="kasd")
+        ent_p=ent_poisk.get()
+        if ent_p in riik_peal:
+            lbl_otvet.config(text=riik_peal[ent_p])
+        elif ent_p in peal_riik:
+            lbl_otvet.configure(text=peal_riik[ent_p])
         else:
-            pass
-
+            lbl_otvet.config(text="Ei leitud")
     riik_peal,peal_riik=sp("riigid_pealinnad.txt")
     aken_poisk=Tk()
     aken_poisk.title("Nimekirja")
     aken_poisk.iconbitmap("kot.ico")
-    var=IntVar()
-    lbl_poisk=Label(aken_poisk,text="Leia pealinn riigi järgi ja vastupidi",bg="lightgray",font="Algerian 25",height=2,width=15)
-    check_riik=Radiobutton(aken_poisk, text="Riik pealinna järgi",font="Arial 20",variable=var,value=1)
-    check_peal=Radiobutton(aken_poisk, text="Pealinn riiki järgi",font="Arial 20",variable=var,value=2)
-    lbl_otvet=Label(aken_poisk,bg="lightgray",font="Algerian 25",height=1,width=5)
+    lbl_poisk=Label(aken_poisk,text="Leia pealinn riigi järgi ja vastupidi",font="Algerian 25",height=2)
+    lbl_otvet=Label(aken_poisk,bg="lightgray",font="Arial 15",height=1,width=10)
     ent_poisk=Entry(aken_poisk, fg="blue",bg="lightblue",width=15,font="Arial 20",justify=CENTER)
     test=Button(aken_poisk,text="test",command=poisk2)
-    check_riik.deselect()
-    check_peal.deselect()
     lbl_poisk.pack()
     lbl_otvet.pack()
     ent_poisk.pack()
-    check_riik.pack()
-    check_peal.pack()
     test.pack()
-    #cr=check_riik.get()
-    #if cr=="Riik":
-    #try:
-    #    if p==1:
-    #        o=input("Sisestage riik: ")
-    #        print(riik_peal[o])
-    #    elif p==2:
-    #        o=input("Sisestage pealinn: ")
-    #        print(peal_riik[o])
-    #except:
-    #    uus("riigid_pealinnad.txt")
 
-#def uus():
-#    r=input("Kirjutage riigi: ")
-#    while r in spis1:
-#        r=input("See riik on ")
-#    p=input("Kirjutage pealinn: ") 
-#    while p in spis2:
-#        p=input("See pealinn on ")
-#    spis1.update({r:p}) 
-#    salvesta()
+def uus(event):
+    global up,ur
+    u=Toplevel()
+    u.title("Uus")
+    u.iconbitmap("kot.ico")
+    lbl_poisk=Label(u,text="Lisage uus riik ja pealinn",font="Algerian 25",height=2)
+    uus_riik=Label(u,text="Sisestage uus riik",font="Arial 20")
+    uus_peal=Label(u,text="Sisestage uus pealinn",font="Arial 20")
+    up=Entry(u,fg="blue",bg="lightblue",width=15,font="Arial 20",justify=CENTER)
+    ur=Entry(u,fg="blue",bg="lightblue",width=15,font="Arial 20",justify=CENTER)
+    salv=Button(u,text="Enter", font="Arial 15", relief=GROOVE)
+    uus_p=up.get()
+    uus_r=ur.get()
+    spis1.update({uus_r:uus_p})
 
+    salv.bind("<Button-1>",salvesta())
+    lbl_poisk.pack()
+    uus_riik.pack()
+    ur.pack()
+    uus_peal.pack() 
+    up.pack()
+    salv.pack()
+    u.mainloop()
 def salvesta():
-    riik=list(spis1.keys())
-    linn=list(spis1.values())
-    y=[]
-    for i in range(len(spis1)):
-        y.append(riik[i]+"-"+linn[i]+"\n")
-    file=open("riigid_pealinnad.txt",'w', encoding="utf-8-sig")
-    file.writelines(y)
+        global up,ur
+        uus_p=up.get()
+        uus_r=ur.get()
+        spis1.update({uus_r:uus_p})
+        riik=list(spis1.keys())
+        linn=list(spis1.values())
+        y=[]
+        riik.append(uus_r) 
+        linn.append(uus_p) 
+        for i in range (len(riik)):
+            y.append(riik[i]+"-"+linn[i]+"\n")
+        file=open("riigid_pealinnad.txt",'w', encoding="utf-8-sig")
+        file.writelines(y)
 
 sonastik={}
 spis1,spis2=sp("riigid_pealinnad.txt")
@@ -90,7 +89,7 @@ aken=Tk()
 aken.title("Euroopa riigid")
 aken.geometry("450x350")
 aken.iconbitmap("kot.ico")
-lbl=Label(aken,text="Euroopa riigid",bg="lightgray",font="Algerian 25",height=2,width=15)
+lbl=Label(aken,text="Euroopa riigid",font="Algerian 25",height=2,width=15)
 nimekirja_btn=Button(aken,text="Vaata nimekirja", font="Arial 15", relief=GROOVE)
 poisk_btn=Button(aken,text="Leia pealinn riigi järgi ja vastupidi", font="Arial 15", relief=GROOVE)
 lisa_btn=Button(aken,text="Lisage uus riik ja pealinn",font="Arial 15", relief=GROOVE)
@@ -98,7 +97,9 @@ parand_btn=Button(aken,text="Parandage riiki/pealinna",font="Arial 15", relief=G
 test_btn=Button(aken,text="Teadmiste kontroll",font="Arial 15", relief=GROOVE)
 
 nimekirja_btn.bind("<Button-1>",vaata_nimekirja)
-#poisk_btn.bind("<Button-1>",poisk)
+poisk_btn.bind("<Button-1>",poisk)
+lisa_btn.bind("<Button-1>",uus)
+
 lbl.pack()
 nimekirja_btn.pack()
 poisk_btn.pack()
